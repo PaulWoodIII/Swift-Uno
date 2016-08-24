c372b0ebef62d3306e6d83dc59f3e348930383dd
Paul Wood III <paul.wood.iii@gmail.com>
2016-08-24 14:08:13 -0400

    editing functions

--- Pages/Functions.xcplaygroundpage/Contents.swift
+++ Pages/Functions.xcplaygroundpage/Contents.swift
@@ -3,20 +3,29 @@
 /*:
  Obviously using a function like print is great but we can write our own functions as well. Usually functions are written to break down complexity into easy to understand parts.
  
- Lets take a step back to remember some geometry. We have determined that the circumfrence of a circle is equal to the two times the radius of the circle times pi. We can write a function that makes that easier for us to find the circumfrence circle without repeating ourselves elsewhere in our code. A function would make things easier to read in the future as well as allows us to reuse the code.
+ Here is a simple function to print hello:
  */
-//: First lets define a constant named pi
-let pi = 3.14159
-
-//: Next lets define the function
-func circumfrence (radius : Double) -> Double {
-    return 2 * radius * pi
+func sayHello(name: String){
+    print("Hello", name);
 }
-//: Lastly we can use the function
-circumfrence(radius: 10.0)
+//: To call the function we just do this:
+sayHello(name: "Tron")
+//: Functions can be broken down into this format
+/*
+ func functionName(ArgumentName : ArgumentType) -> ReturnType {
+   //code
+ }
+ */
+/*:
+ Here are somethings to remember with functions:
+ 
+ * Function names usually start with a lower case letter
+ * Functions don't always need to return something, like `print()`. But if a function says it returns something it must return something.
+ * Function arguments are named by default
+ */
 //: To show how functions can be reused lets define a function to that returns the area of a circle given it's radius
 func area(radius : Double) -> Double {
-    return radius * radius * pi
+    return radius * radius * 3.14159
 }
 area(radius: 10.0)
 //: Now that we have defined area we can use it in other parts of our code. For example we can use the area of a circle to determine the volume of a cylindar.
